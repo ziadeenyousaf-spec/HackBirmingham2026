2d488858296b6fff6cc6ac9f9a35b77eaf5dd59c
joelf987 <83581451+joelf987@users.noreply.github.com>
2026-03-15 10:30:18 +0000

    made a map and added more functionality to player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -6,7 +6,37 @@ var health : int = 100
 var enemyHealth : int = 100
 var is_attacking : bool = false
 #@onready var anim = $AnimatedSprite
+var wood_count : int = 0
+var metal_count : int = 0
+@onready var tile_map = get_node("../TileMap/trees") 
+# Make sure the name matches!
 
+func _input(event):
+	if event.is_action_pressed("interact"): 
+		# Create an "interact" action in Input Map (e.g., 'E')
+		harvest()
+
+func harvest():
+	# 1. Get the tile position under the player
+	var player_tile_pos = tile_map.local_to_map(global_position)
+	
+	# 2. Get the tile data at that spot
+	var data = tile_map.get_cell_tile_data(player_tile_pos)
+	
+	if data:
+		# 3. Check the "type" we set in Step 1
+		var tile_type = data.get_custom_data("type")
+		
+		if tile_type == "Trees":
+			print("You mined a tree!")
+			Inventory.add_item(preload("res://assets/items/wood.tres"))
+			# 4. Remove the tree tile (replace with empty/grass)
+			tile_map.erase_cell(player_tile_pos)
+		elif tile_type == "Props.mine.png":
+			print("You mined some metal!")
+			Inventory.add_item(preload("res://assets/items/metal.tres")) 
+		else:
+			print("Nothing to mine here.")
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right",
